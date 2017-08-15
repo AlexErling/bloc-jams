938c2a244d849250372c6d55c92bfb3e77439b61
Alex Erling <alex.erling@gmail.com>
2017-08-15 16:36:50 -0500

    checkpoint 17 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -10,12 +10,12 @@
 
 var animatePoints = function(points) {
       forEach(points, revealPoint);
-  };
+};
 
 $(window).load(function() {
   if ($(window).height() > 950) {
       animatePoints();
-  }
+  };
 
   var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
 
